db9aa270de4b49f70dbcf8cb8dd8306f4c70f997
Hashemi Rafsan <rafsanhashemi@gmail.com>
2021-07-28 22:56:31 +0600

    change code for deployment

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import faker from 'faker';
 import { random } from 'lodash';
 import {UnControlled as CodeMirror} from 'react-codemirror2';
-require('codemirror/mode/javascript/javascript');
-import Input from './components/Input';
 import Switch from './components/Switch';
-import Output from './components/Output';
+require('codemirror/mode/javascript/javascript');
+
 
 function App() {
-  // const [obj, setObj] = useState({
-  //   locale: 'en',
-  //   type: 'object',
-  //   prop: {
-  //     id: {
-  //       type: 'datatype.uuid'
-  //     },
-  //     name: {
-  //       type: 'lorem.words',
-  //       min: 10,
-  //       max: 15
-  //     },
-  //     comments: {
-  //       type: 'array',
-  //       prop: {
-  //         user: {
-  //           type: 'object',
-  //           prop: {
-  //             id: {
-  //                 type: 'datatype.uuid'
-  //             },
-  //             name: {
-  //                 type: 'name.firstName'
-  //             },
-  //             roles: {
-  //                 type: 'object',
-  //                 prop: {
-  //                     email: {
-  //                         type: 'internet.email'
-  //                     }
-  //                 }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     } 
-  //   }
-  // });
 
   const [obj, setObj] = useState("");
+  const [output, setOutput] = useState("");
 
   const generateFakeJson = () => {
     let defaultObj = {
@@ -61,36 +22,37 @@ function App() {
 
     let mergeObj = Object.assign(defaultObj, obj);
 
-    console.log(_generate_response(mergeObj))
+    setOutput(JSON.stringify(_generate_response(mergeObj), null, 2))
   }
 
   const _generate_response = (obj) => {
     let result = null;
 
-    if (obj.type == 'object') {
+    if (obj.type === 'object') {
         
         if (! obj.prop) return false;
 
         result = {};
 
-        for(var key in obj.prop) {
+        for(let key in obj.prop) {
             result[key] = _generate_response(obj.prop[key]);
         }
 
         return result;
     }
 
-    if (obj.type == 'array') {
-        
+    if (obj.type === 'array') {
+
         if (! obj.prop) return false;
 
         result = [];
 
         let max = obj.total || 1;
 
-        for(let i = 0; i < max; i++) {
+        for(let i = 1; i <= max; i++) {
             let nObjs = {};
-            for(var key in obj.prop) {
+            for(let key in obj.prop) {
+              console.log(key)
                 nObjs[key] = _generate_response(obj.prop[key]);
             }
             result.push(nObjs);
@@ -98,7 +60,7 @@ function App() {
 
         return result;
     }
-
+    console.log(obj.type)
     let parseType = obj.type.split('.');
     let fkr = faker;
 
@@ -122,7 +84,7 @@ function App() {
   }
 
   const cmOptions = {
-    theme: "default",
+    theme: "material",
     height: "auto",
     viewportMargin: Infinity,
     mode: {
@@ -147,26 +109,28 @@ function App() {
           <div className="row">
             <div className="col-md-5 com-sm-12">
             <CodeMirror
-          options={cmOptions}
-          onChange={(editor, data, value) => {
-             try {
-                var jsonStr = value.replace(/(\w+:)|(\w+ :)/g, function(matchedStr) {
-                  return '"' + matchedStr.substring(0, matchedStr.length - 1) + '":';
-                });
-                
-                setObj(JSON.parse(jsonStr))
-             } catch (error) {
-                setObj({})
-             }
-          }}
-        />
-        <button onClick={() => generateFakeJson()}>Render</button>
+                options={cmOptions}
+                onChange={(editor, data, value) => {
+                try {
+                    var jsonStr = value.replace(/(\w+:)|(\w+ :)/g, function(matchedStr) {
+                      return '"' + matchedStr.substring(0, matchedStr.length - 1) + '":';
+                    });
+                    
+                    setObj(JSON.parse(jsonStr))
+                } catch (error) {
+                    setObj({})
+                }
+              }}
+            />
               </div>
             <div className="col-md-2 com-sm-12">
-              <Switch/>
+              <Switch handleGenerateFakeJson={generateFakeJson}/>
             </div>
             <div className="col-md-5 com-sm-12">
-              <Output/>
+            <CodeMirror
+                options={cmOptions}
+                value={output}
+            />
             </div>
           </div>
         </div>
--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Switch() {
+function Switch(props) {
     return (
         <div className="d-flex align-items-center flex-column">
-            <button className="btn btn-success align-self-center my-4">Generate</button>
+            <button className="btn btn-success align-self-center my-4" onClick={() => props.handleGenerateFakeJson()}>Generate</button>
 
             <button className="btn btn-primary align-self-center my-4">Copy Output</button>
         </div>
